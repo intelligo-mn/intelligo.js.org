52b6b8181586aea2541246ef3fb1090ef0927649
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-21 23:57:19 +0800

    actiivty ууд үүсгэсэн

---
+++ app/src/main/java/com/dgltour/CompanyActivity.java
@@ -0,0 +1,13 @@
+package com.dgltour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+    }
+}
