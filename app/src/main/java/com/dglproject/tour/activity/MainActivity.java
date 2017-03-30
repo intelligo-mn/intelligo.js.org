fe87d90951dbdecf216966f51ea0aca337c3c07f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-30 12:25:06 +0800

    доод хэсгийн цэснүүд болон json parser

--- app/src/main/java/com/dglproject/tour/activity/MainActivity.java
+++ app/src/main/java/com/dglproject/tour/activity/MainActivity.java
@@ -1,15 +1,96 @@
 package com.dglproject.tour.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dglproject.tour.R;
+import com.dglproject.tour.fragments.HomeFragment;
+import com.dglproject.tour.widgets.CustomViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private int[] mTabsIcons = {
+            R.drawable.ic_recents_selector,
+            R.drawable.ic_favorite_selector,
+            R.drawable.ic_place_selector,
+            R.drawable.ic_place_selector,
+            R.drawable.ic_place_selector};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomViewPager viewPager = (CustomViewPager) findViewById(R.id.view_pager);
+        MyPagerAdapter pagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+
+        if (viewPager != null) {
+            viewPager.setPagingEnabled(false);
+            viewPager.setAdapter(pagerAdapter);
+        }
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        if (mTabLayout != null) {
+            mTabLayout.setupWithViewPager(viewPager);
+
+            for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+                TabLayout.Tab tab = mTabLayout.getTabAt(i);
+                if (tab != null)
+                    tab.setCustomView(pagerAdapter.getTabView(i));
+            }
+
+            mTabLayout.getTabAt(0).getCustomView().setSelected(true);
+        }
+    }
+
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public final int PAGE_COUNT = 5;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public View getTabView(int position) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_tab, null);
+            ImageView icon = (ImageView) view.findViewById(R.id.icon);
+            icon.setImageResource(mTabsIcons[position]);
+            return view;
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch (pos) {
+
+                case 0:
+                    return HomeFragment.newInstance(1);
+                case 1:
+                    return HomeFragment.newInstance(2);
+                case 2:
+                    return HomeFragment.newInstance(3);
+                case 3:
+                    return HomeFragment.newInstance(4);
+                case 4:
+                    return HomeFragment.newInstance(5);
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
     }
 }
---
+++ app/src/main/java/com/dglproject/tour/fragments/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.dglproject.tour.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dglproject.tour.R;
+
+public class HomeFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+    private int mPageNo;
+
+    public static HomeFragment newInstance(int pageNo) {
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNo);
+        HomeFragment fragment = new HomeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPageNo = getArguments().getInt(ARG_PAGE);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        TextView textView = (TextView) view.findViewById(R.id.text_view);
+        return view;
+    }
+}
