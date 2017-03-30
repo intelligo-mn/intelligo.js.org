29d009daa42f2333a51514ce9ae8d24c17c822ed
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-30 11:37:30 +0800

    login, register, splash activities

---
+++ app/src/main/java/com/dglproject/tour/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+package com.dglproject.tour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
