2706f6f176737147f23b2dc5969eb0e524ba11d8
Doljko Dolly <doljko927@gmail.com>
2017-05-01 01:41:41 +0800

    change application languages

--- app/src/main/java/com/dglproject/tour/activity/SplashScreenActivity.java
+++ app/src/main/java/com/dglproject/tour/activity/SplashScreenActivity.java
@@ -1,10 +1,16 @@
 package com.dglproject.tour.activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.dglproject.tour.R;
+
+import java.util.Locale;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,23 +18,49 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        try {
-            Thread timerThread = new Thread(){
-                public void run(){
-                    try{
-                        sleep(2000);
-                    }catch(InterruptedException e){
-                        e.printStackTrace();
-                    }finally{
-                        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            };
-            timerThread.start();
-        }catch (Exception e){
 
-        }
+        ImageButton en = (ImageButton)findViewById(R.id.langEn);
+        ImageButton mn = (ImageButton)findViewById(R.id.langMn);
+        ImageButton cn = (ImageButton)findViewById(R.id.langCn);
+        ImageButton ru = (ImageButton)findViewById(R.id.langRu);
+
+        en.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("en");
+            }
+        });
+
+        mn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("mn");
+            }
+        });
+
+        cn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("zh");
+            }
+        });
+
+        ru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("ru");
+            }
+        });
+    }
+    private void changeLanguage(String lang) {
+        Resources res = getApplicationContext().getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.locale = new Locale(lang);
+        res.updateConfiguration(conf, dm);
+
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
