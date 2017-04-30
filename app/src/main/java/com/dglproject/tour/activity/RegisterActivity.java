0f30cdbb31a1f3542cd5ee4dc54bf8d1e4a5d8be
Doljko Dolly <doljko927@gmail.com>
2017-04-30 22:59:03 +0800

    res values шинэчилсэн

--- app/src/main/java/com/dglproject/tour/activity/RegisterActivity.java
+++ app/src/main/java/com/dglproject/tour/activity/RegisterActivity.java
@@ -194,7 +194,6 @@ public class RegisterActivity extends AppCompatActivity {
             JSONObject json = jsonParser.makeHttpRequest(URL, "POST", params);
 
             return json;
-
         }
 
         protected void onPostExecute(JSONObject result) {
